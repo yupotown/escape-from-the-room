7cf092063717845bb9f1582ba060381e6c29de4d
Yupo <yupotown@gmail.com>
2019-09-29 21:40:11 +0900

    画像をクリックすると切り替わる

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img id="main" :src="image" @click="switchImage"
+    @mousedown="onMouseDown"/>
+    <pre>{{console}}</pre>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  props: {
+    image: Object
+  },
+  data() {
+    return {
+      current: 1,
+      console: ""
+    }
+  },
+  methods: {
+    switchImage() {
+      if (this.current == 1) {
+        this.current = 2
+      } else {
+        this.current = 1
+      }
+      this.setImage()
+    },
+    onMouseDown(e) {
+      this.consoleLog(`${e.offsetX}, ${e.offsetY}`)
+    },
+    setImage() {
+      if (this.current == 1) {
+        this.image = require('@/assets/kari1.png')
+      } else {
+        this.image = require('@/assets/kari2.png')
+      }
+    },
+    consoleLog(msg) {
+      this.console = `${msg}\n${this.console}`
+    }
+  },
+  created() {
+    this.setImage()
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
